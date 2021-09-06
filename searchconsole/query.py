a4895d621beeff23298fc2ddeb4fcc7c625fa568
francescocan <77925723+francescocan@users.noreply.github.com>
2021-09-06 19:22:22 +0200

    Update query.py

--- searchconsole/query.py
+++ searchconsole/query.py
@@ -337,7 +337,7 @@ class Report:
         self.queries = []
 
         self.dimensions = query.raw.get('dimensions', [])
-        self.metrics = ['clicks', 'impressions', 'ctr', 'position']
+        self.metrics = ['date', 'query', 'clicks', 'impressions', 'ctr', 'position']
         self.columns = self.dimensions + self.metrics
         self.Row = collections.namedtuple('Row', self.columns)
         self.rows = []
